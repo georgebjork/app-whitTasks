5ae4bf934de2fdd880793e58f787e2ea55d85b59
George Bjork <georgebjork@outlook.com>
2021-03-02 17:06:55 -0800

    menu drawer added

--- lib/MyDay.dart
+++ lib/MyDay.dart
@@ -13,13 +13,42 @@ class MyDay extends StatefulWidget {
 class MyDayState extends State<MyDay> {
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(elevation: 0.0,),
       body: Container(
-        width: double.infinity,   //This will expand the width of the container to the whole page
-        padding: EdgeInsets.all(24.0),
-        color: Colors.grey[350],
+        width: double.infinity, //This will expand the width of the container to the whole page
+        padding: EdgeInsets.only(left: 16.0, right: 16.0, bottom: 20.0),
+        color: Colors.grey[900],
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start, //This will shift everything to the left
-          children: [MyDayHeader(), TaskCardWidget(), TaskCardWidget() ]
+            crossAxisAlignment: CrossAxisAlignment
+                .start, //This will shift everything to the left
+            children: [MyDayHeader(), TaskCardWidget(), TaskCardWidget()]),
+      ),
+      
+        drawer: Drawer(
+          child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Drawer Header'),
+              decoration: BoxDecoration(
+                color: Colors.red[900],
+              ),
+            ),
+            ListTile(
+              title: Text('Item 1'),
+              onTap: () {
+                // Update the state of the app.
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+          ],
         ),
       ),
     );

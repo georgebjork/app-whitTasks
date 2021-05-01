caa10e813bb31a073cad09b238a3c916d31d7680
George Bjork <georgebjork@outlook.com>
2021-05-01 15:20:12 -0700

    tasks now register long press

--- lib/Widgets/TaskCardWidget.dart
+++ lib/Widgets/TaskCardWidget.dart
@@ -4,43 +4,58 @@ import '../Classes/Task.dart';
 import 'CheckMarkBox.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
-class TaskCardWidget extends StatelessWidget {
-  
-  Task task; 
+class TaskCardWidget extends StatefulWidget {
+  Task task;
   TaskCardWidget(this.task);
 
   @override
+  TaskCardWidgetState createState() => TaskCardWidgetState(task);
+}
+
+class TaskCardWidgetState extends State<TaskCardWidget> {
+
+  Task task; 
+  TaskCardWidgetState(this.task);
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity, //this makes it take up the whole space in the container
-      padding: EdgeInsets.symmetric(
-          //padding of text in container
-          vertical: 17.0,
-          horizontal: 20.0),
-
-      margin: EdgeInsets.only(
-        bottom: 10.0    //Change the distance between two containers 
-      ),
-      decoration: BoxDecoration(
-        //Change the color and the rounded edges and add 50% opacity 
-        //Theme.of(context).accentColor
-        color: Theme.of(context).cardColor, //.withOpacity(0.6), 
-        borderRadius: BorderRadius.circular(20.0)),
-
-      child: Row( 
-        children: [CheckMarkBox(task), Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[ 
-            Text(task.getTask(), style: TextStyle(color: Colors.grey[300], fontSize: 15, fontWeight: FontWeight.bold, decoration: task.isDone ? TextDecoration.lineThrough : TextDecoration.none )),
-            Row(
-              children: <Widget>[
-                Icon(Icons.calendar_today_sharp, color: Colors.white, size: 14 ),
-                Text(task.getDate(), style: TextStyle(color: Colors.grey[350], fontSize: 12,))
-              ],
-            )
-          ],
-        ),]
+    return GestureDetector(
+      onLongPress: () {
+        print("hello tap");
+      },
+      child: Container(
+        width: double.infinity, //this makes it take up the whole space in the container
+        padding: EdgeInsets.symmetric(
+            //padding of text in container
+            vertical: 17.0,
+            horizontal: 20.0
+          ),
+        margin: EdgeInsets.only(
+          bottom: 10.0    //Change the distance between two containers 
         ),
+        decoration: BoxDecoration(
+          //Change the color and the rounded edges and add 50% opacity 
+          //Theme.of(context).accentColor
+          color: Theme.of(context).cardColor, //.withOpacity(0.6), 
+          borderRadius: BorderRadius.circular(20.0)
+        ),
+        child: Row( 
+          children: [ 
+          CheckMarkBox(task), 
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[ 
+              Text(task.getTask(), style: TextStyle(color: Colors.grey[300], fontSize: 15, fontWeight: FontWeight.bold, decoration: task.isDone ? TextDecoration.lineThrough : TextDecoration.none )),
+              Row(
+                children: <Widget>[
+                  Icon(Icons.calendar_today_sharp, color: Colors.white, size: 14 ),
+                  Text(task.getDate(), style: TextStyle(color: Colors.grey[350], fontSize: 12,))
+                ],
+              )
+            ],
+          ),]
+          ),
+      ),
     );
   }
 }

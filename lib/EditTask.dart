57a800d2725a94698491fd14699e5abc9e774299
George Bjork <georgebjork@outlook.com>
2021-05-08 22:27:18 -0700

    can now change task name

--- lib/EditTask.dart
+++ lib/EditTask.dart
@@ -33,7 +33,6 @@ class EditTaskState extends State<EditTask>{
       body: Container(
         color: Theme.of(context).accentColor,
         width: double.infinity,
-
          child: Consumer<TaskProvider>(
            builder:  (context, provider, child) {
            return Column(
@@ -45,7 +44,7 @@ class EditTaskState extends State<EditTask>{
                 child: Container(
                   width: double.infinity,
                   padding: EdgeInsets.only(left: 30.0, right: 16.0, top: 30.0),
-                  decoration: BoxDecoration(color: Theme.of(context).primaryColor, borderRadius: BorderRadius.only(topRight: Radius.circular(25), topLeft: Radius.circular(25))),
+                  decoration: BoxDecoration(color: Theme.of(context).primaryColor, borderRadius: BorderRadius.only(topRight: Radius.circular(36), topLeft: Radius.circular(36))),
                   child: Column(
                     children: [
                       Status(task),
@@ -64,19 +63,55 @@ class EditTaskState extends State<EditTask>{
   }
 }
 
-class Title extends StatelessWidget{
+class Title extends StatefulWidget{
   Task task;
   Title(this.task);
+  @override
+  TitleState createState() =>TitleState(task);
+}
 
+class TitleState extends State<Title>{
+
+  Task task;
+  TitleState(this.task);
+  final taskController = TextEditingController();
+  
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
        padding: EdgeInsets.only(left: 30.0, right: 16.0, top: 30),
-        child: Text(task.task, style: TextStyle(color: Colors.white, fontSize: 35, fontWeight: FontWeight.bold)),
+        child: TextField(
+          controller: taskController,
+          style: TextStyle(fontSize: 35, color: Colors.white, fontWeight: FontWeight.bold),
+          decoration: InputDecoration(
+            hintText: task.task,
+            hintStyle: TextStyle(fontSize: 35, color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          onTap: () {
+            taskController.text = task.task;
+            FocusScopeNode currentFocus = FocusScope.of(context);
+            //print("focus");
+            if (!currentFocus.hasPrimaryFocus) {
+              currentFocus.unfocus();
+              print(taskController.text);
+              //print("Unfocus");
+              //context.read<TaskProvider>().update(task);
+            }
+          },
+
+          textInputAction: TextInputAction.go,
+            onSubmitted: (text) {
+              task.task = text;
+              context.read<TaskProvider>().update(task);
+            },
+        )
+        //Text(task.task, style: TextStyle(color: Colors.white, fontSize: 35, fontWeight: FontWeight.bold)),
       );
     
     //Text(task.task, style: TextStyle(color: Colors.white, fontSize: 18,));
   }
+
+  
 }
 
 class DueDate extends StatelessWidget{
